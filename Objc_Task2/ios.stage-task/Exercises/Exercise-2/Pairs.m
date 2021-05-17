1747cec813d40227636c5f2d3359140f274dfa08
AnastasiaSkoryukova <anastasia_skoryukova@mail.ru>
2021-05-17 20:17:02 +0300

    add answer to task 2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,29 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    
+    NSInteger numberOfElements = [array count];
+    NSInteger pairs = 0;
+    
+    
+    for (int i = 0; i < numberOfElements - 1; i++) {
+        
+        if ([[array objectAtIndex:i+1] intValue] - [[array objectAtIndex:i] intValue] == [number intValue]) {
+            pairs += 1;
+        }
+        if (i > 0) {
+            if ([[array objectAtIndex:i+1] intValue] - [[array objectAtIndex:i-1] intValue] == [number intValue]) {
+                pairs += 1;
+            }
+    }
+    }
+    
+
+    
+    return pairs;
 }
 
 @end
+
+
+
--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,61 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSMutableString *string;
+    NSMutableString *finalString = [NSMutableString stringWithString:@""];
+    NSMutableArray *numbersArrayMutable = [NSMutableArray arrayWithArray:numbersArray];
+    NSNumber *defaultNumber = [[NSNumber alloc] init];
+    defaultNumber = [NSNumber numberWithInt:0];
+    int i = 0;
+    
+    if (numbersArray != nil && [numbersArray count] != 0) {
+            
+            if ([numbersArray count] < 4) {
+                long lastElement = [numbersArray indexOfObject:[numbersArray lastObject]];
+                for (long j = lastElement + 1; j < 4; j++) {
+                    [numbersArrayMutable insertObject:defaultNumber atIndex:j];
+                }
+                numbersArray = [numbersArrayMutable copy];
+            }
+                
+                for (NSNumber *number in numbersArray) {
+                    
+                    
+                    
+                    if ([number intValue] <= 255 && [number intValue] >= 0) {
+                        string = [NSMutableString stringWithFormat:@"%d", [number intValue]];
+                    }
+                    
+                    if (i < [numbersArray count] - 1) {
+                        [string appendString:@"."];
+                    }
+                    
+                   
+                    
+                    if ([finalString length] <= 13) {
+                        [finalString appendString:string];
+                    }
+                   
+                    if ([number intValue] > 255) {
+                        finalString = [NSMutableString stringWithString:@"The numbers in the input array can be in the range from 0 to 255."];
+                    }
+                        
+                        if ([number intValue] < 0)  {
+                        finalString = [NSMutableString stringWithString:@"Negative numbers are not valid for input."];
+                    }
+                    i++;
+                    
+                }
 }
-
-@end
+            
+        
+        return finalString;
+    }
+    
+    
+    
+    @end
+    
+    
+    
